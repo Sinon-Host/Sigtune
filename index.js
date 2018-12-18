a129aeaa4cedac8b28b90f5570cabd81446b4609
RosieCode95 <rosiecode@outlook.com>
2018-12-18 23:53:33 +0000

    fixing repo

---
+++ index.js
@@ -0,0 +1,8 @@
+// Client stuff
+const Client = require('./src/client/Client');
+const ShardManager = require('./src/client/ShardManager');
+
+module.exports = {
+    Client,
+    ShardManager,
+};
--- src/index.js
+++
@@ -1,8 +0,0 @@
-// Client stuff
-const Client = require('./client/Client');
-const ShardManager = require('./client/ShardManager');
-
-module.exports = {
-    Client,
-    ShardManager,
-};
